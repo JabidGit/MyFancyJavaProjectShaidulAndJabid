1986a91486af1d3d3f4a2d0e70b7baba4186b7eb
ShaidulGit <mislam163062@bscse.uiu.ac.bd>
2022-01-11 22:54:01 +0600

    branchY created, singleton implemented, im Y

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
-public class Main {
+public class SingleObject {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+   //create an object of SingleObject
+   private static SingleObject instance = new SingleObject();
+
+   //make the constructor private so that this class cannot be
+   //instantiated
+   private SingleObject(){}
+
+   //Get the only object available
+   public static SingleObject getInstance(){
+      return instance;
+   }
+
+   public void showMessage(){
+      System.out.println("Hello World!");
+   }
 }
